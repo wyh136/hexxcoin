db22b5558c51c0015b3455c5330345823491189c
hexxcointakeover <hexx.fix@post.com>
2017-01-29 13:54:58 +0200

    total coins

--- src/main.h
+++ src/main.h
@@ -56,7 +56,7 @@ static const int64 DUST_SOFT_LIMIT = 10000; // 0.0001
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1000; // 0.00001
 /** No amount larger than this (in ztoshi) is valid */
-static const int64 MAX_MONEY = 21000000 * COIN;
+static const int64 MAX_MONEY = 9999999 * COIN;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
